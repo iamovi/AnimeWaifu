b8e13c4fc2ebe4f4c60d38a4f1fac254f2c0b8f8
Maruf OVi <137372623+iamovi@users.noreply.github.com>
2024-02-05 02:32:39 +0600

    vat to let/const

--- assets/app.js
+++ assets/app.js
@@ -1,34 +1,34 @@
-var swipeContainer = document.getElementById('swipe-container');
-var preloader = document.getElementById('preloader');
-var hammer = new Hammer(swipeContainer);
-var isLoading = false;
+const swipeContainer = document.getElementById('swipe-container');
+const preloader = document.getElementById('preloader');
+const hammer = new Hammer(swipeContainer);
+let isLoading = false;
 
 hammer.get('swipe').set({ direction: Hammer.DIRECTION_ALL });
 
 function getRandomWaifuImage(category) {
   if (isLoading) {
-    return; // Avoid generating a new image while the current one is loading
+    return;
   }
 
-  preloader.style.display = 'block'; // Show preloader
-  swipeContainer.style.backgroundImage = 'url(https://ik.imagekit.io/iamovi/uwu.jpg?updatedAt=1707075866067)'; // Show new image during loading
-  swipeContainer.classList.add('loading'); // Add loading class
+  preloader.style.display = 'block';
+  swipeContainer.style.backgroundImage = 'url(https://ik.imagekit.io/iamovi/uwu.jpg?updatedAt=1707075866067)';
+  swipeContainer.classList.add('loading');
   isLoading = true;
 
-  var image = new Image();
+  const image = new Image();
   image.src = '';
 
   image.onload = function() {
     swipeContainer.style.backgroundImage = 'url(' + image.src + ')';
-    preloader.style.display = 'none'; // Hide preloader once image is fully loaded
-    swipeContainer.classList.remove('loading'); // Remove loading class
-    isLoading = false; // Set loading status to false
+    preloader.style.display = 'none';
+    swipeContainer.classList.remove('loading');
+    isLoading = false;
   };
 
   fetch(`https://api.waifu.pics/sfw/${category}`)
     .then(response => response.json())
     .then(data => {
-      image.src = data.url; // Set the source after fetching to trigger the 'load' event
+      image.src = data.url;
     });
 }
 
