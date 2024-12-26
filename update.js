5f9e072b8c9b13e4db22eec94d2e014ffff301d7
Maruf Ovi <imgoinin.iamovi@gmail.com>
2024-12-26 12:58:44 +0600

    update version check link

--- update.js
+++ update.js
@@ -5,7 +5,7 @@ async function checkForUpdates() {
     try {
         // Append a unique query parameter to prevent caching
         const cacheBuster = `?t=${Date.now()}`;
-        const response = await fetch(`./version.json${cacheBuster}`);
+        const response = await fetch(`https://iamovi.github.io/AnimeWaifu/version.json${cacheBuster}`);
         const data = await response.json();
 
         // Compare the current app version with the one from version.json
