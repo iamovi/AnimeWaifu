3e63f45709fd374c22e4664f94bec377e7a03393
Maruf Ovi <imgoinin.iamovi@gmail.com>
2025-01-02 16:12:51 +0600

    add poster

--- qc.js
+++ qc.js
@@ -4,3 +4,12 @@
         const baseUrl = "https://animewaifuqc.netlify.app";
         iframe.src = `${baseUrl}?cacheBust=${new Date().getTime()}`;
     });
+    
+    
+    document.addEventListener('DOMContentLoaded', function () {
+        const iframe = document.getElementById('HenIframe');
+        const baseUrl = "https://www.hentaicity.com";
+        iframe.src = `${baseUrl}?cacheBust=${new Date().getTime()}`;
+    });
+    
+    
