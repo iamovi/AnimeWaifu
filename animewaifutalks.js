ecc2d30b0a954bf8377eed6d6ff59dbe8944a9eb
Maruf Ovi <137372623+iamovi@users.noreply.github.com>
2025-01-16 14:58:40 +0600

    Update animewaifutalks.js

--- animewaifutalks.js
+++ animewaifutalks.js
@@ -17,10 +17,14 @@ document
     iframeContainer.style.display = "block";
     preloaderX.style.display = "block";
 
-    loadingText.style.display = "block";
-    loadingText.innerHTML = "Loading...<br> Currently this App's backend is deployed on render.com free tier due to money issues, So the first load may take up to 1 minute. Kindly wait :(";
+    // Delay the loading text by 1 second
+    setTimeout(function() {
+      loadingText.style.display = "block";
+      loadingText.innerHTML = "Loading...<br> Currently this App's backend is deployed on render.com free tier due to money issues, So the first load may take up to 1 minute. Kindly wait :(";
+    }, 1000); // 1 second delay for loading text
 
     iframe.addEventListener("load", function () {
+      // Hide both preloader gif and loading text immediately
       preloaderX.style.display = "none";
       loadingText.style.display = "none";
     });
@@ -36,4 +40,4 @@ document.getElementById("close-iframe").addEventListener("click", function () {
   iframe.src = "";
   preloaderX.style.display = "none";
   loadingText.style.display = "none";
-})
+});
