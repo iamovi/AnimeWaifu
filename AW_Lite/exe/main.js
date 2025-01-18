22981de99b9c246e7e5fa7a4cc281d09a553d88c
Maruf Ovi <imgoinin.iamovi@gmail.com>
2025-01-18 21:41:26 +0600

    update aw lite exe

--- AW_Lite/exe/main.js
+++ AW_Lite/exe/main.js
@@ -1,53 +1,40 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
-let mainWindow;
+function createWindow () {
+    const mainWindow = new BrowserWindow({
+        width: 410,
+        height: 700,
+        resizable: false, // Disable resizing and maximizing
+        autoHideMenuBar: true, // Automatically hide the menu bar
+        webPreferences: {
+            contextIsolation: true,
+            enableRemoteModule: false,
+            nodeIntegration: false,
+        },
+        icon: path.join(__dirname, 'build', 'icon.ico'), // Specify the icon path
+    });
 
-const createWindow = () => {
-  mainWindow = new BrowserWindow({
-    width: 400,
-    height: 700,
-    skipTaskbar: true,
-    show: false, // Do not show window immediately
-    fullscreenable: false, // Disable fullscreen button
-    fullscreen: false, // Ensure the window is not fullscreen by default
-    maximizable: false, // Disable maximize button
-    autoHideMenuBar: true, // Hide the menu bar by default
-    icon: path.join(__dirname, 'build', 'icon.ico'), // Set the icon path to the build directory
-    resizable: false, // Disable resizing
-    webPreferences: {
-      nodeIntegration: false, // Disable Node.js integration for security
-      contextIsolation: true, // Enable context isolation for better security
-    }
-  });
+    mainWindow.setMenuBarVisibility(false); // Explicitly hide the menu bar
 
-  // Load the URL or file that should be shown in the window
-  mainWindow.loadURL('file://' + path.join(__dirname, 'public', 'index.html'));
+    mainWindow.loadFile(path.join(__dirname, 'public', 'index.html'));
 
-  // Show the window when it's ready
-  mainWindow.once('ready-to-show', () => {
-    mainWindow.show();
-  });
+    // Open DevTools (optional)
+    // mainWindow.webContents.openDevTools();
+}
 
-  // Handle window close
-  mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
-};
+app.whenReady().then(() => {
+    createWindow();
 
-// Create window when Electron app is ready
-app.on('ready', createWindow);
-
-// Quit the app when all windows are closed (except for macOS)
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
 });
 
-// Recreate window when clicking on app icon in macOS (when no window is open)
-app.on('activate', () => {
-  if (mainWindow === null) {
-    createWindow();
-  }
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
